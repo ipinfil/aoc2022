6a2ecc4341ad3de00d545b7d7f4068a04f6beae9
Filip Lajčin <filip.lajcin@riesenia.com>
2022-12-01 21:34:29 +0100

    main - 1. level | part 2

---
+++ 1-2/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+)
+
+var numericRegex = regexp.MustCompile(`^[0-9]+$`)
+
+func isNumeric(value string) bool {
+	return numericRegex.MatchString(value)
+}
+
+func main() {
+	file, err := os.Open("1-2/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	actualElf := 0
+	var caloriesPerElf []int
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		if text == "" {
+			caloriesPerElf = append(caloriesPerElf, actualElf)
+			actualElf = 0
+		} else if isNumeric(text) {
+			caloryValue, err := strconv.Atoi(text)
+			if err != nil {
+				continue
+			}
+
+			actualElf += caloryValue
+		}
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(caloriesPerElf)))
+	caloriesPerElf = caloriesPerElf[0:3]
+	result := 0
+
+	for i := 0; i < len(caloriesPerElf); i++ {
+		result += caloriesPerElf[i]
+	}
+
+	fmt.Println(result)
+}
